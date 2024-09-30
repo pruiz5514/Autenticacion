3e1c73011ff6ec405a0091eaa269ec4b07203024
pruiz5514 <pablo_ruiza@hotmail.com>
2024-09-30 08:54:21 -0500

    se hace lo de roles

--- auth.ts
+++ auth.ts
@@ -9,4 +9,21 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
     adapter: PrismaAdapter(db),
     session: { strategy: "jwt" },
     ...authConfig,
+
+    callbacks: {
+        // jwt() se ejecuta cada vez que se crea o actualiza un token JWT
+        // Aca se puede agregar informacion adicional al token
+        jwt({ token, user }) {
+            if (user) { // User is available during sign-in
+                token.role = user.role
+            }
+            return token
+        },
+        session({ session, token }) {
+            if (session.user) {
+                session.user.role = token.role
+            }
+            return session
+        },
+    },
 })
--- src/app/(protected)/admin/page.tsx
+++ src/app/(protected)/admin/page.tsx
@@ -1,7 +1,14 @@
-const Admin = () => {
-    return (
-      <div>page</div>
-    )
+import { auth } from "../../../../auth"
+
+const Admin = async() => {
+  const session = await auth()
+ 
+  if (session?.user?.role !== "admin") {
+    return <div>You are not an admin</div>
   }
+  return (
+      <div>page</div>
+  )
+}
   
   export default Admin
